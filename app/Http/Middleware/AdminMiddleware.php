6f1fa5623f709bb8411d4dca0a964412a7860975
Asiri4784 <adrianus.me.mentor@gmail.com>
2016-04-21 04:47:46 +0700

    added Admin Middleware and ApiAuth

---
+++ app/Http/Middleware/AdminMiddleware.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Auth;
+
+class AdminMiddleware
+{
+    public function handle($request, Closure $next)
+    {
+        if(Auth::user()->role=='admin'){
+            return $next($request);
+        }else{
+            return back();
+        }
+
+    }
+}
+?>
