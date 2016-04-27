39592bad2f0ccd32b62ad08d4957703f1faabf4d
izzunnaqi <izzunnaqi@gmail.com>
2016-04-27 19:24:37 +0700

    fixing views wrong routing

--- resources/views/user/listuser.blade.php
+++ resources/views/user/listuser.blade.php
@@ -101,7 +101,7 @@
                         <div class="title_right">
                             <div class="col-md-5 col-sm-5 col-xs-12 form-group pull-right top_search">
                                 <div class="input-group">
-                                <form action="{{URL::to('/searchadmin/')}}" method = "POST">
+                                <form action="{{URL::to('/searchuser/')}}" method = "POST">
                                   {!! csrf_field() !!}
                                     <input type="text" class="form-control" name="keyword" placeholder="Search User by username...">
                                     <span class="input-group-btn">
