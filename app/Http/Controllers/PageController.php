045b82e2903f4d0738a58c2af0c7d2af39a812d9
Asiri4784 <adrianus.me.mentor@gmail.com>
2016-05-26 03:46:40 +0700

    Fixing registration bugs.

--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -4,7 +4,6 @@ use App\Http\Controllers\Controller;
 use Auth;
 use Hash;
 use App\Models\Person;
-use App\Models\Kategori;
 use App\Models\Product;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
@@ -21,18 +20,6 @@ class PageController extends Controller
     {
         $users = Person::where('role','=','user')->get();
         return view('user.listuser', compact('users'));
-    }
-
-    public function getDashboard2()
-    {
-        $kategories = Kategori::where('id_kategori','>',0)->get();
-        return view('kategori.listkategori', compact('kategories'));
-    }
-    
-    public function getDashboard3()
-    {
-        $products = Product::where('product_id','>',0)->get();
-        return view('product.listproduct', compact('products'));
     }
     //returns edit adminpage with pre-filled parameters
     public function editAdmin($id)
@@ -71,7 +58,7 @@ class PageController extends Controller
     
     }
     //check admin email
-   public function adminEmailChecker(String $request)
+    public function adminEmailChecker(String $request)
     {
         $emailquery = Person::where('email','=',$request)->first();
         if(!is_null($emailquery))
@@ -80,7 +67,7 @@ class PageController extends Controller
         }
         else
         {
-            return false;
+            return false;   
         }
     }
     //saves admin parameters
@@ -88,22 +75,28 @@ class PageController extends Controller
     {
             $admin_username=$request->input('username');
             $admin_name=$request->input('nama');
-            $admin->nama=$admin_name;
-            $role="admin";
-            $admin->role=$role;
             $admin_email=$request->input('email');
-            $admin->email=$admin_email;
+      
             $admin_password=Hash::make($request->input('password'));
-            $admin->password=$admin_password;
-            $admin->username=$admin_username;
-            $admin->save();
+            $role="admin";
+            
+            if(PageController::adminEmailChecker($admin_email)||PageController::adminUnameChecker($admin_username)){
+                return redirect()->route('createadmin')->withErrors('An admin with that email or username has already exists!');       
+            }else{
+                $admin->role=$role;
+                $admin->email=$admin_email;
+                $admin->nama=$admin_name;
+                $admin->password=$admin_password;
+                $admin->username=$admin_username;
+                $admin->save();
+                return redirect()->route('createadmin')->withErrors('An new admin has just been added!');     
+            }         
     }
     //saves the new admin into the database including the parameters
     public function registerAdmin(Request $request)
     {
             $newadmin = new Person;
-            PageController::adminSaver($newadmin, $request);
-            return redirect()->route('dashboard')->withErrors('An new admin has just been added!'); 
+            return PageController::adminSaver($newadmin, $request);
     }
     //search admin with username like input
     public function personSearch(Request $request)
@@ -128,7 +121,7 @@ class PageController extends Controller
             $newadmin = new Person;
             PageController::adminSaver($newadmin,$request);
         
-        } else if($keyword==$admin_username) {
+        }else if($keyword==$admin_username){
             if(PageController::adminEmailChecker($admin_email)&&!!$admin_username==Person::where('email','=',$request)->first()){   
                 return redirect()->route('editadmin',array('id'=>$keyword))->withErrors('An admin with that email has already exists!');       
             }
@@ -138,21 +131,19 @@ class PageController extends Controller
         return redirect()->route('dashboard')->withErrors('An admin profile has just been updated!');
     }
 
-    public function saveUser(Request $request)
-    {
+       public function saveUser(Request $request)
+{
         $keyword=$request->input('olduname');
         $user_username=$request->input('username');
-        if($keyword!=$user_username)
-        {
+       if($keyword!=$user_username){
              $unamecheck=$request->input('username');
              $emailcheck=$request->input('email');
              $unamequery = Person::where('username','=',$unamecheck)->first();
              $emailquery = Person::where('email','=',$emailcheck)->first();
-            if(!is_null($unamequery)||!is_null($emailquery))
-            {   
-                return redirect()->route('edituser',array('id'=>$keyword))->withErrors('An user with that username or email has already exists!');             
+             if(!is_null($unamequery)||!is_null($emailquery))
+             {   
+                 return redirect()->route('edituser',array('id'=>$keyword))->withErrors('An user with that username or email has already exists!');             
             }
-            
             $olduser = Person::where('username','=',$keyword)->first();
             $olduser->delete();
             $newuser = new Person;
@@ -168,15 +159,12 @@ class PageController extends Controller
             $newuser->username=$user_username;
             $newuser->save();
         }
-        else if($keyword==$user_username){
-            $emailcheck=$request->input('email');
-            $emailquery = Person::where('email','=',$emailcheck)->first();
-            
-            if(!is_null($emailquery)&&!!$user_username==Person::where('email','=',$request)->first())
-            {   
+         else if($keyword==$user_username){
+             $emailcheck=$request->input('email');
+             $emailquery = Person::where('email','=',$emailcheck)->first();
+             if(!is_null($emailquery)&&!!$user_username==Person::where('email','=',$request)->first()){   
                  return redirect()->route('edituser',array('id'=>$keyword))->withErrors('An user with that email has already exists!');             
-            }
-            
+             }
             $olduser = Person::where('username','=',$keyword)->first();
             $user_name=$request->input('nama');
             $olduser->nama=$user_name;
@@ -189,234 +177,53 @@ class PageController extends Controller
         }
         return redirect()->route('dashboard')->withErrors('An user profile has just been updated!');
     }
-    
-    public function editUser($id)
+     public function editUser($id)
     {
-        $result= Person::where('username','=',$id)->first();
-        return view('user.edituser', compact('result')); 
+        
+             $result= Person::where('username','=',$id)->first();
+             return view('user.edituser', compact('result'));
+         
     }
-    
     public function createUser()
     {
         return view('user.adduser');
     }
-    
     public function deleteuser($id)
     {
         $result= Person::where('username','=',$id)->first();
         $result->delete();
         return redirect()->route('dashboard')->withErrors('An new user has just been deleted!');
-    }
-    
-    public function searchUser(Request $request)
-    {
-        $username=$request->get('keyword');
-        $users = Person::where('username','like','%'.$username.'%')->where('role','=','user')->get();
-        return view('user.listuser', compact('users')); 
-    }
-    
-    public function registerUser(Request $request)
-    {
-        $newuser = new Person;
-        $user_name=$request->input('nama');
-        $newuser->nama=$user_name;
-        $role="user";
-        $newuser->role=$role;
-        $user_email=$request->input('email');
-        $newuser->email=$user_email;
-        $user_password=Hash::make($request->input('password'));
-        $newuser->password=$user_password;
-        
-        $user_username=$request->input('username');
-        $newuser->username=$user_username;
-        $unamecheck=$request->input('username');
-        $emailcheck=$request->input('email');
-        $unamequery = Person::where('username','=',$unamecheck)->first();
-        $emailquery = Person::where('email','=',$emailcheck)->first();
-        if(!is_null($unamequery)||!is_null($emailquery))
-        {   
-            return redirect()->route('createuser')->withErrors('An admin with that username or email has already exists!');             
-        }
-        $newadmin->save();
-        return redirect()->route('dashboard')->withErrors('An new User has just been added!'); 
-    }
-
-    public function saveKategori(Request $request)
-    {
-        $keyword=$request->input('olduname');
-        $keyword2=$request->input('oldid');
-        $user_username=$request->input('nama');
-        if($keyword!=$user_username)
-        {
-            $unamecheck=$request->input('nama');
-             
-            $unamequery = Kategori::where('nama','=',$unamecheck)->first();
-            
-            if(!is_null($unamequery))
-            {   
-                return redirect()->route('editkategori',array('nama'=>$keyword))->withErrors('An user with that username or email has already exists!');             
-            }
-
-            $olduser = Kategori::where('nama','=',$keyword)->first();
-            $olduser->delete();
-            $newuser = new Kategori;
+}
+public function searchUser(Request $request)
+     {
+         $username=$request->get('keyword');
+         $users = Person::where('username','like','%'.$username.'%')->where('role','=','user')->get();
+         return view('user.listuser', compact('users')); 
+     }
+public function registerUser(Request $request)
+{
+            $newuser = new Person;
             $user_name=$request->input('nama');
             $newuser->nama=$user_name;
-            $newuser->id_kategori=$keyword2;
-            $newuser->save();
-        }
-        else if($keyword==$user_username)
-        {          
-            $olduser = Kategori::where('nama','=',$keyword)->first();
-            $user_name=$request->input('nama');
-            $olduser->nama=$user_name;
-           
-            $olduser->save();
-        }
-        return redirect()->route('dashboard')->withErrors('An user profile has just been updated!');
-    }
-
-    public function editKategori($id)
-    {
-        $result= Kategori::where('id_kategori','=',$id)->first();
-        return view('kategori.editkategori', compact('result'));    
-    }
-    
-    public function createKategori()
-    {
-        return view('kategori.addkategori');
-    }
-    
-    public function deleteKategori($id)
-    {
-        $result= Kategori::where('id_kategori','=',$id)->first();
-        $result->delete();
-        return redirect()->route('dashboardkategori')->withErrors('An new user has just been deleted!');
-    }
-
-    public function searchKategori(Request $request)
-    {
-        $username=$request->get('keyword');
-        $kategories = Kategori::where('nama','like','%'.$username.'%')->get();
-        return view('kategori.listkategori', compact('kategories')); 
-    }
-
-    public function registerKategori(Request $request)
-    {
-        $newkategori = new Kategori;
-        $nama_kategori=$request->input('nama');
-        $newkategori->nama=$nama_kategori;
-        
-       
-        $kategoricheck=$request->input('nama');
-        $unamequery = Kategori::where('nama','=',$kategoricheck)->first();
-         
-        if(!is_null($unamequery))
-        {   
-            return redirect()->route('createkategori')->withErrors('An kategori with that name has already exists!');             
-        }
-        
-        $newkategori->save();
-        return redirect()->route('dashboardkategori')->withErrors('An new kategoi has just been added!'); 
-    }
-    
-    public function saveProduct(Request $request)
-    {
-        $keyword=$request->input('olduname');
-        $keyword2=$request->input('oldstock');
-        $keyword3=$request->input('oldid');
-        $user_username=$request->input('nama');
-
-        if($keyword!=$user_username)
-        {
-            $unamecheck=$request->input('nama');
-             
-            $unamequery = Kategori::where('nama','=',$unamecheck)->first();
+            $role="user";
+            $newuser->role=$role;
+            $user_email=$request->input('email');
+            $newuser->email=$user_email;
+            $user_password=Hash::make($request->input('password'));
+            $newuser->password=$user_password;
             
-            if(!is_null($unamequery))
-            {   
-                 return redirect()->route('editproduct',array('nama'=>$keyword))->withErrors('An product with that username or email has already exists!');             
-            }
-         
-            $olduser = Product::where('nama','=',$keyword);
-            $olduser->delete();
-            $newuser = new Product;
-            $user_name=$request->input('nama');
-            $harga_product=$request->input('harga');
-            $stock_product=$request->input('stock');
-            $kategori_product=$request->input('kategori');
-            $foto_product=$request->input('foto');
-            $newuser->nama=$user_name;
-            $newuser->harga=$harga_product;
-            $newuser->foto=$foto_product;
-            $newuser->id_kategori=$kategori_product;
-            $newuser->product_id=$keyword3;
-    
-            $newuser->save();
-        }
-        
-        else if($keyword==$user_username)
-        { 
-            $olduser = Person::where('nama','=',$keyword)->first();
-            $user_name=$request->input('nama');
-            $olduser->nama=$user_name;
-           
-            $olduser->save();
-        }
-        return redirect()->route('dashboardproduct')->withErrors('An user profile has just been updated!');
-    }
-
-    public function editProduct($id)
-    {
-        $result= Product::where('product_id','=',$id)->first();
-        return view('product.editproduct', compact('result'));
-    }
-
-    public function createProduct()
-    {
-        return view('product.addproduct');
-    }
-
-    public function deleteProduct($id)
-    {
-        $result= Product::where('product_id','=',$id);
-        $result->delete();
-        return redirect()->route('dashboardproduct')->withErrors('An new product has just been deleted!');
-    }
-
-    public function searchProduct(Request $request)
-    {
-        $username=$request->get('keyword');
-        $products = Product::where('nama','like','%'.$username.'%')->get();
-        return view('product.listproduct', compact('products')); 
-    }
-    
-    public function registerProduct(Request $request)
-    {
-        $newproduct = new Product;
-        $nama_product=$request->input('nama');
-
-        $harga_product=$request->input('harga');
-        $kategori_product=$request->input('kategori');
-        //$stock_product=$request->input('stock');
-        $gambar_product=$request->input('foto');
-
-
-        $newproduct->nama=$nama_product;
-        $newproduct->harga=$harga_product;
-        $newproduct->id_kategori=$kategori_product;
-        //$newproduct->stock=$stock_product;
-        $newproduct->foto=$gambar_product;
-
-        $productcheck=$request->input('nama');
-        $unamequery = Product::where('nama','=',$productcheck)->first();
-
-        if(!is_null($unamequery))
-        {   
-            return redirect()->route('createproduct')->withErrors('An product with that name has already exists!');             
-        }
-        $newproduct->save();
-        return redirect()->route('dashboard')->withErrors('An new kategoi has just been added!'); 
-    }
+            $user_username=$request->input('username');
+            $newuser->username=$user_username;
+            $unamecheck=$request->input('username');
+             $emailcheck=$request->input('email');
+             $unamequery = Person::where('username','=',$unamecheck)->first();
+             $emailquery = Person::where('email','=',$emailcheck)->first();
+             if(!is_null($unamequery)||!is_null($emailquery))
+             {   
+                 return redirect()->route('createuser')->withErrors('An admin with that username or email has already exists!');             
+             }
+              $newadmin->save();
+              return redirect()->route('dashboard')->withErrors('An new User has just been added!'); 
+}
 
 }
